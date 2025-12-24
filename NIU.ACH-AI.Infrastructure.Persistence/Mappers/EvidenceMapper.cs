ce49d0b2e08e5f5f214b239efb7637b5045fb5dc
Tim S <soderstromtj@gmail.com>
2025-12-23 19:12:32 -0500

    Improve evidenceId assignment robustness in mapper
    Updated ToDatabase to check for both Guid.Empty and empty string
    when assigning evidenceId, ensuring a new Guid is generated
    when appropriate and preventing invalid Guid usage.

--- NIU.ACH-AI.Infrastructure.Persistence/Mappers/EvidenceMapper.cs
+++ NIU.ACH-AI.Infrastructure.Persistence/Mappers/EvidenceMapper.cs
@@ -16,8 +16,8 @@ public static class EvidenceMapper
         DomainEntity.Evidence domain,
         Guid stepExecutionId)
     {
-        // First check if domain.EvidenceId is not empty, if it is not empty, we should try to parse it into a Guid
-        Guid evidenceId = string.IsNullOrEmpty(domain.EvidenceId.ToString()) 
+        // First check if domain.EvidenceId is not empty or Guid.Empty, if it is not empty, we should try to parse it into a Guid
+        Guid evidenceId = Guid.Empty.Equals(domain.EvidenceId) || string.IsNullOrEmpty(domain.EvidenceId.ToString()) 
             ? Guid.NewGuid() 
             : Guid.TryParse(domain.EvidenceId.ToString(), out var parsedGuid) ? parsedGuid : Guid.NewGuid();
 
